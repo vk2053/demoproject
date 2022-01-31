bf9502d3589d38a108891501b0493697ac3ed2dc
Vivek <vk943074@gmail.com>
2022-01-31 15:10:17 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Form from "./components/Form";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-//import Home from './components/Home';
+
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route exact path="/" element={<Form />}  />
+        <Route exact path="/home" element={<Home />}  />
+        
         
       </Routes>
     </BrowserRouter>
